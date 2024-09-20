87c92f9ce5f10af3bb9bb372a94b6844d0f0e41f
fauzihamad <fauzihamad23@gmail.com>
2024-09-20 16:59:18 +0700

    update

--- resources/views/user/layouts/partials/header.blade.php
+++ resources/views/user/layouts/partials/header.blade.php
@@ -1,10 +1,17 @@
-<div class="flex px-8 items-center py-4 bg-white rounded-b-lg shadow-lg">
-  <div class="flex gap-4 justify-self-start flex-none">
-    <a href="javascript:;" class="font-bold italic text-md">Home</a>
-    <a href="javascript:;" class="font-light italic text-md">Project</a>
+<div class="flex items-center py-4 bg-white rounded-b-lg shadow-lg relative">
+  
+  <div class="flex justify-between w-full px-8">
+    <div class="flex gap-4 justify-self-start flex-none">
+      <a href="javascript:;" class="font-bold italic text-md">Home</a>
+      <a href="javascript:;" class="font-lightgit a text-md">Project</a>
+    </div>
+  
+    <div class="flex">
+      asd
+    </div>
   </div>
 
-  <div class="flex flex-1 gap-2 justify-self-center justify-center">
+  <div class="flex flex-1 gap-2 justify-self-center justify-center absolute mx-auto w-full">
     <img src="{{asset('assets/images/logo/logo-whatsapp.svg')}}" alt="">
     <img src="{{asset('assets/images/logo/logo-upwork.svg')}}" alt="">
     <img src="{{asset('assets/images/logo/logo-linkedin.svg')}}" alt="">
@@ -12,8 +19,6 @@
     <img src="{{asset('assets/images/logo/logo-instagram.svg')}}" alt="">
   </div>
 
-  <div class="flex">
-    asd
-  </div>
+  
 
 </div>
